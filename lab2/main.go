97e90951cdce290799e137d043168d64c1c94f0d
J Boyd Trolinger <boyd.trolinger@gmail.com>
2021-07-14 16:30:29 -0700

    Added lab2

---
+++ lab2/main.go
@@ -0,0 +1,52 @@
+// Lab 2 -- types and variables
+//
+// Programmer name: __________
+// Date completed:  __________
+
+package main
+
+import "fmt"
+
+func main() {
+  // NOTE: you should use fmt.Printf with appropriate printing "verbs" for all
+  //       of your outputs
+
+	// 1. Declare a string variable and assign it the value of the current day of the week
+
+
+  // 2. Declare a string variable and assign it the value of the current month
+
+
+  // 3. Declare an integer variable and assign it the value of the current date
+
+
+  // 4. Declare an integer variable and assign it the value of the current year
+
+
+  // 5. Print a message: "Today is DAY, MONTH DATE, YEAR."
+	//    (fill in DAY/MONTH/DATE/YEAR with the appropriate variables)
+  //    EXAMPLE: Today is Monday, July 12, 2021.
+
+
+  // 6. Declare a string variable and assign it the value of your home town
+
+
+  // temperature variable
+  var temperature float64 = 99.5
+
+	// 7. Print a message: "The temperature in CITY is currently TEMPERATURE degrees."
+	//    (fill in CITY/TEMPERATURE with the appropriate variables)
+  //    EXAMPLE: The temperature in Chico is currently 99.5 degrees.
+
+
+  // 8. Re-assign the value of your home town variable with a different town or city
+
+
+  // 9. Re-assign the value of the temperature variable with a different temperature
+
+
+  // 10. Print a message: "The temperature in CITY is currently TEMPERATURE degrees."
+	//     (fill in CITY/TEMPERATURE with your variables)
+  //     EXAMPLE: The temperature in Chicago is currently 79.0 degrees.
+
+}

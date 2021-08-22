78ff580480b1f7bddb0a787d10ba0c8c16c58be8
Boyd Trolinger <boyd.trolinger@gmail.com>
2021-08-22 13:48:21 -0700

    Updated unit test in lab14

--- lab14/main_test.go
+++ lab14/main_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+const floatTolerance = 0.001
+
 func TestCartItemStruct(t *testing.T) {
 	item := CartItem{name: "lemon", price: 0.25, quantity: 2}
 
@@ -12,7 +14,7 @@ func TestCartItemStruct(t *testing.T) {
 		t.Errorf("Expected name to be 'lemon', got '%s'", item.name)
 	}
 
-	if item.price != 0.25 {
+	if math.Abs(item.price - 0.25) > floatTolerance {
 		t.Errorf("Expected price to be 0.25, got %.2f", item.price)
 	}
 
@@ -37,9 +39,8 @@ func TestCartItemCost(t *testing.T) {
 
 	total := CartItemCost(item)
 	expectedTotal := 0.50
-	tolerance := 0.001
 
-	if math.Abs(expectedTotal - total) > tolerance {
+	if math.Abs(expectedTotal - total) > floatTolerance {
 		t.Errorf("Expected %.2f, got %.2f", expectedTotal, total)
 	}
 }
